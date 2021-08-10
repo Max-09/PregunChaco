7b88ee5e2fb24454e317a6c5ab1cc6b22b2c9fd6
Maximiliano Rodriguez <maxiarod09@gmail.com>
2021-08-10 16:11:35 -0300

    Proyecto Nuevo

---
+++ pregunchaco/pregunchaco/settings/local.py
@@ -0,0 +1,15 @@
+from .base import *
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'sql_server.pyodbc',
+        'NAME': '',
+        'Trusted_Connection':'yes',
+        'HOST':'localhost\\SQLEXPRESS01',
+        'OPTIONS':{
+        	'driver':'SQL Server Native Client 11.0',
+        }
+    }
+}
