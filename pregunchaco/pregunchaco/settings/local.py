ac4d358791280874caa40727dfd0b368a234f10c
Javier <xavierea@gmail.com>
2021-08-11 14:15:27 -0300

    "arreglo a gitignore"

--- pregunchaco/pregunchaco/settings/local.py
+++ pregunchaco/pregunchaco/settings/local.py
@@ -5,11 +5,11 @@ DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'sql_server.pyodbc',
-        'NAME': 'JUEGO',
+        'NAME': 'PregunChaco',
         'Trusted_Connection':'yes',
-        'HOST':'localhost\\SQLEXPRESS01',
-        'OPTIONS':{
-        	'driver':'SQL Server Native Client 11.0',
+        'HOST': 'localhost\\SQLEXPRESS',
+        'OPTIONS': {
+            'driver': 'SQL Server Native Client 11.0',
         }
     }
 }
