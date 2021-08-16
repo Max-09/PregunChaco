18eeb4f55f2f406f0e18a8817221e40110c37215
Max-09 <81917083+Max-09@users.noreply.github.com>
2021-08-16 15:00:27 -0300

    Agregue los templates(No actualizados)

--- pregunchaco/pregunchaco/urls.py
+++ pregunchaco/pregunchaco/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pregunchaco.views import Main, Login, Game
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('Main/', Main),
+    path('Login/', Login),
+    path('Game/', Game),
 ]
