eb33154c33e262708b893546147e8bdd872f23a1
Javier <xavierea@gmail.com>
2021-08-22 20:18:51 -0300

    arreglo visual del register.html
    Co-Authored-By: GiuliBuyatti <89216498+GiuliBuyatti@users.noreply.github.com>

--- pregunchaco/apps/usuarios/forms.py
+++ pregunchaco/apps/usuarios/forms.py
@@ -3,11 +3,15 @@ from django.contrib.auth.forms import UserCreationForm
 from . import models
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
-    #contraseña1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
-    #contraseña2 = forms.CharField(label='Confirma Contraseña', widget=forms.PasswordInput)
-
+    #username = forms.CharField(label='Usuario', widget=forms.CharField(attrs={'class': 'input-field', 'placeholder':'USUARIO', 'required':'required'}))
+    email = forms.EmailField(widget= forms.EmailInput(attrs={'class': 'input-field', 'placeholder':'CORREO', 'required':'required'}))
+    password1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput(attrs={'class': 'input-field', 'placeholder':'CONTRASEÑA', 'required':'required'}))
+    password2 = forms.CharField(label='Confirma Contraseña', widget=forms.PasswordInput(attrs={'class': 'input-field', 'placeholder':'REINGRESAR CONTRASEÑA', 'required':'required'}))
+   
     class Meta:
         model = models.Usuario
         fields = ['username', 'email', 'password1', 'password2']
         help_text = {k:"" for k in fields}
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'input-field', 'placeholder':'USUARIO', 'required':'required'}),
+                        }
