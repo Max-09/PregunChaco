8fabb05fb245df959f3636ebe39d386eaed23b23
Javier <xavierea@gmail.com>
2021-09-02 18:06:12 -0300

    desaupdate wsgy.py
    Co-Authored-By: Max-09 <81917083+Max-09@users.noreply.github.com>

--- pregunchaco/pregunchaco/wsgi.py
+++ pregunchaco/pregunchaco/wsgi.py
@@ -8,27 +8,9 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-import sys
 
-# assuming your Django settings file is at '/home/myusername/mysite/mysite/settings.py'
-path = '/home/PregunChaco/Pregunchaco'
-if path not in sys.path:
-    sys.path.insert(0, path)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'PregunChaco.settings.local.py'
-
-## Uncomment the lines below depending on your Django version
-###### then, for Django >=1.5:
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pregunchaco.settings')
+
 application = get_wsgi_application()
-###### or, for older Django <=1.4
-#import django.core.handlers.wsgi
-#application = django.core.handlers.wsgi.WSGIHandler()
-
-
-
-    # from django.core.wsgi import get_wsgi_application
-
-    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pregunchaco.settings')
-
-    # application = get_wsgi_application()
